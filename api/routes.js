8c9c1735a73c347e1593979ae96fdfda5d560147
adankot <adankot@gmail.com>
2016-08-15 21:19:48 +0200

    Routes added for example controller

--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,21 @@
 'use strict';
 
+// Express
 const express = require('express');
-
 const router = express.Router();
 
+// Controllers
+const example = require('./controllers/example');
+
+// Routes
 router.get('/', function(req, res){
   res.send('Hello Wolrd!');
 });
+
+// get examples
+router.get('/examples', example.list);
+
+// create example by query string. URL: localhost:3000/examples/create?name=Banana&price=20
+router.get('/examples/create', example.create);
 
 module.exports = router;
