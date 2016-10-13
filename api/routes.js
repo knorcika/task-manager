4b7edd4b5779ab41e230c2f072c3e0215676d5ce
Kéri Norbert <knorbi3@gmail.com>
2016-10-13 19:46:12 +0200

    Remove profile and task from models and controllers

--- api/routes.js
+++ api/routes.js
@@ -6,8 +6,6 @@ const router = express.Router();
 
 // Controllers
 const example = require('./controllers/example');
-const task = require('./controllers/task');
-const profile = require('./controllers/profile');
 
 // Routes
 router.get('/', function(req, res){
@@ -34,13 +32,5 @@ router.put('/examples/:exampleId', example.update);
 // type: delete
 // URL: localhost:3000/examples/57c18c90caa2e6141fab4962
 router.delete('/examples/:exampleId', example.delete);
-
-router.get('/profile', profile.list);
-
-router.get('/profile/create', profile.create);
-
-router.get('/task', task.list);
-
-router.get('/task/create', task.create);
 
 module.exports = router;
